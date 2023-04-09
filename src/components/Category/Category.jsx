d65a04f1a48119b1f42f5f8d8eb74bbd582ff993
shaaheensaab <shaaheensaab@gmail.email>
2023-04-09 17:56:51 +0600

    meal card component updated

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -3,11 +3,15 @@ import React from 'react';
 const Category = ({ meal }) => {
     const { idCategory, strCategory, strCategoryDescription, strCategoryThumb } = meal;
     return (
-        <div className='bg-cyan-50 border border-cyan-300 rounded shadow-sm my-2'>
+        <div className='bg-cyan-50 border border-cyan-300 rounded shadow-sm px-5 py-2'>
             <img className='w-32 mx-auto' src={strCategoryThumb} alt="" />
-            <div className='px-5'>
+            <div className=''>
                 <h1 className='font-bold text-xl bg-cyan-100 px-1'>{strCategory}</h1>
-                <p>{strCategoryDescription}</p>
+                <p className='text-slate-800 py-2'>{strCategoryDescription}</p>
+            </div>
+            <div className='flex gap-2'>
+                <button className='bg-cyan-400 w-[50%] px-3 py-1 text-white font-bold rounded-l-full'>Add to cart</button>
+                <button className='bg-cyan-400 w-[50%] px-3 py-1 text-white font-bold rounded-r-full'>Buy Now</button>
             </div>
         </div>
     );
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,8 +10,8 @@ const Home = () => {
 
     return (
         <div>
-            <h1 className='font-bold bg-slate-100 p-1 text-center'>Categories</h1>
-            <div className='grid md:grid-cols-2 gap-2 px-5'>
+            <h1 className='font-bold bg-slate-100 p-1 text-center md:text-lg'>Meal Categories</h1>
+            <div className='container mx-auto grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 px-5 py-2'>
                 {
                     meals.map(meal => <Category
                         key={meal.idCategory}
