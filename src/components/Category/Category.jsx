f714f9edeaca866107966f79c7824cda437fe69b
shaaheensaab <shaaheensaab@gmail.email>
2023-04-09 18:07:21 +0600

    meal card btn modified

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -3,15 +3,17 @@ import React from 'react';
 const Category = ({ meal }) => {
     const { idCategory, strCategory, strCategoryDescription, strCategoryThumb } = meal;
     return (
-        <div className='bg-cyan-50 border border-cyan-300 rounded shadow-sm px-5 py-2'>
-            <img className='w-32 mx-auto' src={strCategoryThumb} alt="" />
-            <div className=''>
-                <h1 className='font-bold text-xl bg-cyan-100 px-1'>{strCategory}</h1>
-                <p className='text-slate-800 py-2'>{strCategoryDescription}</p>
+        <div className='flex flex-col justify-between bg-cyan-50 border border-cyan-300 rounded shadow-sm px-5 py-3'>
+            <div>
+                <img className='w-[50%] mx-auto' src={strCategoryThumb} alt="" />
+                <div className=''>
+                    <h1 className='font-bold text-xl bg-cyan-100 px-1'>{strCategory}</h1>
+                    <p className='text-slate-600 py-2'>{strCategoryDescription.slice(0, 120)}...</p>
+                </div>
             </div>
             <div className='flex gap-2'>
-                <button className='bg-cyan-400 w-[50%] px-3 py-1 text-white font-bold rounded-l-full'>Add to cart</button>
-                <button className='bg-cyan-400 w-[50%] px-3 py-1 text-white font-bold rounded-r-full'>Buy Now</button>
+                <button className='bg-cyan-400 w-[50%] px-3 py-1 text-white font-extrabold rounded'>Add to cart</button>
+                <button className='bg-cyan-400 w-[50%] px-3 py-1 text-white font-extrabold rounded'>Buy Now</button>
             </div>
         </div>
     );
