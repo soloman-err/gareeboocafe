11e712be232960a90be7e68137ea9cc6355dd258
shaaheensaab <shaaheensaab@gmail.email>
2023-04-09 18:12:24 +0600

    meal card modified

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 const Category = ({ meal }) => {
     const { idCategory, strCategory, strCategoryDescription, strCategoryThumb } = meal;
     return (
-        <div className='flex flex-col justify-between bg-cyan-50 border border-cyan-300 rounded shadow-sm px-5 py-3'>
+        <div className='flex flex-col justify-between bg-cyan-100 border border-cyan-300 rounded-md shadow-sm px-5 py-3'>
             <div>
                 <img className='w-[50%] mx-auto' src={strCategoryThumb} alt="" />
                 <div className=''>
                     <h1 className='font-bold text-xl bg-cyan-100 px-1'>{strCategory}</h1>
-                    <p className='text-slate-600 py-2'>{strCategoryDescription.slice(0, 120)}...</p>
+                    <p className='text-slate-600 py-2'>{strCategoryDescription.substring(0, 100)}..
+                        <button className='text-cyan-700'>Read more</button>
+                    </p>
                 </div>
             </div>
             <div className='flex gap-2'>
