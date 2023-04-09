b9d4dd12ecf9a04f58264dc9d145b8a17ac30a0f
shaaheensaab <shaaheensaab@gmail.email>
2023-04-09 16:08:17 +0600

    Navbar UI modified

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ import { faHamburger, faX } from '@fortawesome/free-solid-svg-icons';
 const Header = () => {
     const [open, setOpen] = useState(false);
     return (
-        <nav className='flex md:flex-row justify-between bg-cyan-600 py-5 px-6 lg:px-36 font-bold md:text-xl text-white items-center'>
+        <nav className='flex flex-row-reverse md:flex-row justify-between bg-cyan-600 py-5 px-6 lg:px-36 font-bold md:text-xl text-white items-center'>
             <div className=''>
                 <h1 className='font-extrabold md:text-2xl uppercase'>gareeboocafe</h1>
             </div>
@@ -20,7 +20,7 @@ const Header = () => {
                 </div>
 
                 <div className='md:space-x-5 md:flex-row uppercase justify-center'>
-                    <ul className={`absolute md:static flex flex-col md:flex-row duration-300 bg-cyan-600 md:bg-transparent py-5 space-y-3 md:space-y-0 gap-4 px-5 ${open ? 'right-0' : '-right-52'}`}>
+                    <ul className={`absolute md:static flex flex-col md:flex-row duration-300 bg-cyan-600 md:bg-transparent py-5 space-y-3 md:space-y-0 gap-4 px-5 ${open ? 'left-0' : '-left-52'}`}>
                         <Link to='/'>Home</Link>
                         <Link to='/cart'>Cart</Link>
                         <Link to='/blog'>Blog</Link>
